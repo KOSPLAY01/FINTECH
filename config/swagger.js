1a5c89ccc2aeca20e9a5e28eaa7c51b1916a19bf
Abednego Alex <abednegoalex1@gmail.com>
2025-07-17 16:45:22 +0100

    fixed swagger url

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'https://fintech-dun.vercel.app',
+        url: 'https://finflow-blond.vercel.app',
       },
       {
         url: 'http://localhost:3000',
